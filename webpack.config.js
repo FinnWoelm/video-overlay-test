fc5bfe011c3d40292a6a60a6b57ae15d4ff921ae
Finn Woelm <public@finnwoelm.com>
2020-04-05 01:39:05 +0200

    Upgrade webpack to v4
    Fix critical security loopholes with webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const webpack = require('webpack');
+const path = require('path')
 
 module.exports = {
   entry: {
     index: './src/index.js',
   },
   output: {
-    path: './js/',
+    path: path.join(__dirname, 'js'),
     filename: '[name].bundle.js',
   },
   devtool: 'source-map',
   module: {
-    loaders: [
-      { test: /\.css$/, loader: 'style!css' },
+    rules: [
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.svg$/, loader: 'url-loader' },
     ],
   },
